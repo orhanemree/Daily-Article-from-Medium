547583359862ba848cadb121ed1fe518823d41b0
Orhan Emre <orhanemre.dev@gmail.com>
2021-11-04 23:52:53 +0300

    added links and tags in email texts

--- main.py
+++ main.py
@@ -4,64 +4,74 @@ from datetime import datetime
 import requests
 import smtplib
 
-# get page source code with requests
-tag = "sports"
-url = f"https://medium.com/tag/{tag}"
-page = requests.get(url)
-
-# parse source code with bs4
-soup = BeautifulSoup(page.text, "html.parser")
-
-# get last article about tag
-articles = soup.select("#root > div > div.t.u > div > div > div.l.ao > div")[1].select(".l > div", limit=1)
-
-# get last article's author name
-author = articles[0].select("h4")[0].text
-
-# get last article's title
-title = articles[0].select("h2")[0].text
-
-# get last article's description
-description = articles[0].select("h3")[0].text
-
-# try to get last article's image
-try:
-    image = articles[0].select("img")[4]
-except IndexError:
-    try:
-        image = articles[0].select("img")[3]
-    except IndexError:
-        try:
-            image = articles[0].select("img")[2]
-        except IndexError:
-            try:
-                image = articles[0].select("img")[1]
-            except IndexError:
-                image = articles[0].select("img")[0]
-
-# download image
-img_data = requests.get(image["src"]).content
-with open("image.jpeg", "wb") as file:
-    file.write(img_data)
-
-# email settings
 env = dict(dotenv_values(".env"))
 gmail_user = env["gmail_user"]
 gmail_password = env["gmail_password"]
 
 client_names = env["client_names"].split(",")
 client_emails = env["client_emails"].split()
+client_tags = env["client_tags"].split()
 
+# loop for send special email everyone in list "client_emails"
 for i in range(len(client_names)):
+
+    # get page source code with requests
+    client_tag = client_tags[i]
+    url = f"https://medium.com/tag/{client_tag}"
+    page = requests.get(url)
+
+    # parse source code with bs4
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    # get last article about tag
+    articles = soup.select("#root > div > div.t.u > div > div > div.l.ao > div")[1].select(".l > div", limit=1)
+
+    # get last article's author name
+    author = articles[0].select("h4")[0].text
+
+    # get last article's title
+    title = articles[0].select("h2")[0].text
+
+    # get last article's description
+    description = articles[0].select("h3")[0].text
+
+    # get last article's link
+    link = articles[0].select("a")[3]["href"]
+    if "http" in link:
+        pass
+    else:
+        link = f"https://medium.com{link}"
+
+    # try to get last article's image
+    try:
+        image = articles[0].select("img")[4]
+    except IndexError:
+        try:
+            image = articles[0].select("img")[3]
+        except IndexError:
+            try:
+                image = articles[0].select("img")[2]
+            except IndexError:
+                try:
+                    image = articles[0].select("img")[1]
+                except IndexError:
+                    image = articles[0].select("img")[0]
+
+    # download image
+    img_data = requests.get(image["src"]).content
+    with open("image.jpeg", "wb") as file:
+        file.write(img_data)
 
     client_name = client_names[i]
     client_email = client_emails[i]
 
     message = f"Subject: Daily Article from Medium\n" \
               f"Hello again {client_name}! Take a look at the todays article.\n" \
+              f"Tag: {client_tag}\n" \
               f"Author: {author}\n" \
               f"Title: {title}\n" \
-              f"Description: {description}\n"
+              f"Description: {description}\n" \
+              f"Read full article here: {link}"
 
     # get current date dd/mm/YY H:M:S
     current_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
